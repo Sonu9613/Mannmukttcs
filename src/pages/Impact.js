c0b434a093bf34c8b52479c0249c044c57893eb3
Nikhil Garg <nikhil.garg.2509@gmail.com>
2025-01-04 22:26:52 +0530

    our impact

--- src/pages/Impact.js
+++ src/pages/Impact.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import OurImpactChoosing from '../components/OurImpactChoosing'
 
 const Impact = () => {
     return (
-        <>
-            <div>Impact</div>
-            <OurImpactChoosing />
-        </>
+        <div>Impact</div>
     )
 }
 
